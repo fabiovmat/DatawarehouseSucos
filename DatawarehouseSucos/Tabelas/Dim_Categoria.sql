3867da92a4c749bd23cadceb0fc18fc158bc6632
fabiovmat <fabio.meihua08@gmail.com>
2020-12-28 23:01:06 -0300

    Adicionar arquivos de projeto.

---
+++ DatawarehouseSucos/Tabelas/Dim_Categoria.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Dim_Categoria]
+(
+	[Cod_Categoria] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Desc_Categoria] NVARCHAR(200
+	) NULL
+)
---
+++ DatawarehouseSucos/Tabelas/Fato_001.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Fato_001]
+(
+	[Cod_Cliente] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Cod_Produto] NVARCHAR(50) NULL, 
+    [Cod_Organizacional] NVARCHAR(50) NULL, 
+    [Cod_Fabrica] NVARCHAR(50) NULL, 
+    [Cod_Dia] NVARCHAR(50) NULL, 
+    [Faturamento] FLOAT NULL, 
+    [Imposto] FLOAT NULL, 
+    [Custo_Variavel] FLOAT NULL, 
+    [Quantidade_Vendida] FLOAT NULL
+)
